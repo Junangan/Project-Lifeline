9b78b6258513d67a0fd84b841a6ab189e108608a
Gan Jun An <62781947+Junangan@users.noreply.github.com>
2021-03-02 08:16:53 +0800

    Update Record_CRS_Staff.php

--- PHP/Record_CRS_Staff.php
+++ PHP/Record_CRS_Staff.php
@@ -1,6 +1,6 @@
 <?php 
 //declaration and initialization
-$UserName = $_POST['name'];
+$UserName = $_POST['username'];
 $Password = $_POST['password'];
 $Name = $_POST['realName'];
 $PhoneNum = $_POST['phoneNumber'];
@@ -20,27 +20,13 @@ if($conn->connect_error){
 
 $conn->select_db("CRS");
 
-//using try to check the table is create or no,if no then will create the relate table
-$GetStaffQuery = "SELECT * from Staff";
-$result = $conn->query($GetUserQuery);
-if(!$result){
-	$GetUserQuery = "SELECT * from User";
-	$result = $conn->query($GetUserQuery);
-	if(!$result){
-		$CreateUserTableQuery = "CREATE TABLE User(username VARCHAR(255) PRIMARY KEY,password VARCHAR(255) NOT NULL,name VARCHAR(255) NOT NULL,phone integer(11) NOT NULL)";
-		$conn->query($CreateUserTableQuery);
-	}
-	$CreateStaffTableQuery = "CREATE TABLE Volunteer(StaffID integer(10) AUTO_INCREMENT PRIMARY KEY,position VARCHAR(255) NOT NULL,joinDate DATE NOT NULL,username VARCHAR(255) NOT NULL,foreign key(username) references User(username))";
-	$conn->query($CreateStaffTableQuery);
-}
-
 //to check the username is dublicate or no, if no then will add the user
 $GetUserQuery = "SELECT * from User where username='$UserName'";
 $result = $conn->query($GetUserQuery);
-if(!$result){
+if($result->num_rows > 0){
 	echo "<script>
 	alert('The user name is duplicate , please select another user name');
-	window.location.href='../Record_Test_Centre_Officer.php';
+	window.location.href='../Record_CRS_Staff.html';
 	</script>";
 }
 else{
@@ -50,6 +36,7 @@ else{
 	$conn->query($AddVolunteerQuery);
 	echo "<script>
 		alert('Record Tester Successfully');
+		window.location.href='../Record_CRS_Staff.html';
 		</script>";
 }
 
